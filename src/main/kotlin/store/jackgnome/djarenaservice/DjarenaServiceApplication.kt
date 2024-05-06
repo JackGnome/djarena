19386a5fcca6b0070b555d7108fc5c19c01ba84e
mkovalenkov <mkovalenkov@naumen.ru>
2024-05-07 02:41:18 +0300

    init

---
+++ src/main/kotlin/store/jackgnome/djarenaservice/DjarenaServiceApplication.kt
@@ -0,0 +1,11 @@
+package store.jackgnome.djarenaservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DjarenaServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<DjarenaServiceApplication>(*args)
+}
---
+++ src/test/kotlin/store/jackgnome/djarenaservice/DjarenaServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package store.jackgnome.djarenaservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DjarenaServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
